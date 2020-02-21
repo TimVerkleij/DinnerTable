b534cb02c0a890e05ffaa090f1e1ad26757c4e60
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-02-21 10:15:35 +0100

    Some testing in HTML

--- app/src/main/java/com/example/dinnertable/MainActivity.kt
+++ app/src/main/java/com/example/dinnertable/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 //        webview.settings.domStorageEnabled = true
         webview.loadUrl("file:///android_asset/index.html")
 
-//        webview.loadUrl("https://www.google.com")
+//        webview.loadUrl("https://145.89.154.203:5500")
 
 
         account.setOnClickListener {
