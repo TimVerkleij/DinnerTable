95b1f7161742c87d692ab2ac9ab16ae98878e8a1
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-03-02 16:46:52 +0100

    Optimize imports

--- app/src/main/java/com/example/dinnertable/MainActivity.kt
+++ app/src/main/java/com/example/dinnertable/MainActivity.kt
@@ -5,14 +5,12 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
