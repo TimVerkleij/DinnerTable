5d63b65e64c8253e81b208447e86d621e288a8fa
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-03-09 16:38:21 +0100

    Add firestore connection

--- app/src/main/assets/scripts/addFood.js
+++ app/src/main/assets/scripts/addFood.js
@@ -1,17 +1,3 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBJaWwzG5HTWIXOxywshVpRRHo1i4Gey8s",
-    authDomain: "dinnertable-56c1c.firebaseapp.com",
-    databaseURL: "https://dinnertable-56c1c.firebaseio.com",
-    projectId: "dinnertable-56c1c",
-    storageBucket: "dinnertable-56c1c.appspot.com",
-    messagingSenderId: "297554476203",
-    appId: "1:297554476203:web:928e5939d0765ad2658bee",
-    measurementId: "G-MSFBS5XHBR"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 // Get a reference to the database service
 var database = firebase.database();
 
@@ -41,4 +27,14 @@ function addFood() {
 
     firebase.database().ref('Gerechten').child(searchField.value).child('datum').set(today)
     searchField.value = ''
+}
+
+function showFirestore() {
+    db.collection("gerechten").doc("SF")
+        .onSnapshot(function(doc) {
+            console.log("Current data: ", doc.data());
+        });
+    return firebase.database().ref('Gerechten').once('value').then(function(snapshot) {
+        console.log(snapshot.val())
+    });
 }
