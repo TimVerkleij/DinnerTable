1e810ba248c6c6c9f7a08bbbb3e73c7d5bd4af31
KoenieFrankenstein <koen.frankena@student.hu.nl>
2020-03-18 13:08:08 +0100

    Dynamische changes spinningwheel

--- app/src/main/assets/scripts/stw.js
+++ app/src/main/assets/scripts/stw.js
@@ -1,3 +1,4 @@
+// https://codepen.io/deab/pen/gObXawr
 var button = document.getElementById("button")
 var numberInput = document.getElementById("amount")
 var slots = 3
@@ -12,8 +13,15 @@ window.data = [
 var editDiv = document.getElementById("editDiv")
 var wheelDiv = document.getElementById("wheelDiv")
 var suggestiesImages = document.getElementById("suggestiesImages")
+var outcomeLabel = document.getElementById("label")
+suggestiesImages.style.display = "none"
+var clickToTurn = document.getElementById("clickWheel")
 
 main()
+
+var wheelChart = document.getElementById("spinningWheel")
+console.log(wheelChart)
+
 function main() {
     var padding = { top: 20, right: 30, bottom: 5, left: 30 },
         w = 280 - padding.left - padding.right,
@@ -23,11 +31,12 @@ function main() {
         oldrotation = 0,
         picked = 100000
 
-    var svg = d3.select('#chart')
+    var svg = d3.select("#chart")
         .append("svg")
         .data([data])
         .attr("width", w + padding.left + padding.right)
-        .attr("height", h + padding.top + padding.bottom);
+        .attr("height", h + padding.top + padding.bottom)
+        .attr("id", "spinningWheel")
     var container = svg.append("g")
         .attr("class", "chartholder")
         .attr("transform", "translate(" + (w / 2 + padding.left) + "," + (h / 2 + padding.top) + ")")
@@ -35,7 +44,7 @@ function main() {
         .append("g")
 
     var pie = d3.layout.pie().sort(null).value(function (d) { return 1; });
-    var arc = d3.svg.arc().outerRadius(r + 10);
+    var arc = d3.svg.arc().outerRadius(r + 20);
     var arcs = vis.selectAll("g.slice")
         .data(pie)
         .enter()
@@ -45,7 +54,7 @@ function main() {
 
     //console.log(arcs)
     arcs.append("path")
-        .attr("fill", function (d, i) { return '#F7F7F7' })                 //the background color
+        .attr("fill", function (d, i) { return "#F7F7F7" })                 //the background color
         .attr("d", function (d) { return arc(d); })
 
     arcs.append("text").attr("stroke", "#3C3C3D").attr("transform", function (d) {
@@ -82,13 +91,21 @@ function main() {
                 label.innerHTML = data[picked].label
 
                 container.on("click", spin);
+
+                if (outcomeLabel != '') {
+                    window.suggestiesImages.style.display = "block"
+                }
             });
+
+        clickToTurn.innerHTML = "Draai opnieuw"
+        clickToTurn.style.fontSize = "12px"
+
     }
     //make arrow
     svg.append("g")
         .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h / 2) + padding.top) + ")")
         .append("path")
-        .attr("d", "M-" + (r * .15) + ",0L0," + (r * .05) + "L0,-" + (r * .05) + "Z")
+        .attr("d", "M-" + (r * .20) + ",0L0," + (r * .12) + "L0,-" + (r * .12) + "Z")
         .style({ "fill": "#3C3C3D" });
 
     function rotTween(to) {
@@ -163,7 +180,12 @@ function changeSlots() {
         chart.remove()
         editDiv.style.display = "none"
         wheelDiv.style.display = "block"
-        window.suggestiesImages.style.display = "block"
+
+        if (outcomeLabel.innerHTML != "") {
+            suggestiesImages.style.display = "block"
+        } else if (outcomeLabel.innerHTML == "") {
+            suggestiesImages.style.display = "none"
+        }
         main()
     }
 
