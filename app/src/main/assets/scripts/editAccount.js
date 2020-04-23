a4ed19859ce7f1e676248ce862e5f579e5f40488
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-04-23 16:08:40 +0200

    Enable user to edit info

---
+++ app/src/main/assets/scripts/editAccount.js
@@ -0,0 +1,15 @@
+var editUserPhoto = document.getElementById("editUserPhoto")
+var editName = document.getElementById("editName")
+var editEmail = document.getElementById("editEmail")
+var editBirthDay = document.getElementById("editBirthDay")
+var editAllergies = document.getElementById("editAllergies")
+var editPreferences = document.getElementById("editPreferences")
+
+function showFields() {
+    console.log(editUserPhoto.src)
+    console.log(editName.value)
+    console.log(editEmail.value)
+    console.log(editBirthDay.value)
+    console.log(editAllergies.value)
+    console.log(editPreferences.value)
+}
