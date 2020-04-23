b407cd01ca91bb481ea1db7d7b23fdba97318ffa
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-04-23 11:43:30 +0200

    Display userInfo on first launch

--- app/src/main/assets/scripts/account.js
+++ app/src/main/assets/scripts/account.js
@@ -9,26 +9,51 @@ function closeEditInfo() {
 }
 //? Open en sluit het scherm om userInfo te bewerken //
 
+var userProfileImage = document.getElementById("userPhoto")
+var userNameField = document.getElementById("userName")
+var userEmailField = document.getElementById("userEmail")
+
+var editName = document.getElementById("editName")
+var editEmail = document.getElementById("editEmail")
+var editBirthDay = document.getElementById("editBirthDay")
+var editAllergies = document.getElementById("editAllergies")
+var editPreferences = document.getElementById("editPreferences")
+
+editName.value = "test"
+editEmail.value = "test"
+editAllergies.value = "test"
+editPreferences.value = "test"
+
 var docRef = db.collection("userProfiles").doc(userID);
 
 docRef.get().then(function(doc) {
     if (doc.exists) {
         console.log("Document data:", doc.data());
         db.collection("userProfiles").doc(userID)
-        .onSnapshot(function(doc) {
-        
-        //get profile info from database
-        
-        
-        })
+            .onSnapshot(function(doc) {
+
+                //get profile info from database
+
+
+            })
     } else {
         console.log("No such document!");
 
 
         if (typeof Android != 'undefined') {
             //? console.log(userID, userEmail, userPhoto, userName)
-        
+
             //create document for new user
+
+            userProfileImage.src = userPhoto
+            userNameField.innerHTML = userName
+            userEmailField.innerHTML = userEmail
+
+            editName.value = userName
+            editEmail.value = userEmail
+            // editAllergies.value = "test"
+            // editPreferences.value = "test"
+
 
             let userInfo = {
                 Name: userName,
@@ -36,17 +61,17 @@ docRef.get().then(function(doc) {
                 Photo: userPhoto,
                 ID: userID
             }
-        
+
             let user = {
                 [userID]: userInfo
             }
-        
+
             function uploadData() {
                 db.collection("userProfiles").doc(userID).set({
                     user
                 }, { merge: true })
             }
-        
+
             uploadData()
         }
 
@@ -57,5 +82,3 @@ docRef.get().then(function(doc) {
 }).catch(function(error) {
     console.log("Error getting document:", error);
 });
-
-
