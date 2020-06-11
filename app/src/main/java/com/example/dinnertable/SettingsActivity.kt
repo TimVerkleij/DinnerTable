4c27f4e2d391e321e5d3a6ece98ac47eda023d52
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-06-11 17:17:57 +0200

    Send notification on settings page

--- app/src/main/java/com/example/dinnertable/SettingsActivity.kt
+++ app/src/main/java/com/example/dinnertable/SettingsActivity.kt
@@ -1,17 +1,23 @@
 package com.example.dinnertable
 
+import android.app.Notification
+import android.app.Notification.Builder
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -85,8 +91,7 @@ class SettingsActivity : AppCompatActivity() {
 
 
 
-
-
+        createNotificationChannel()
 
 
 
@@ -120,6 +125,62 @@ class SettingsActivity : AppCompatActivity() {
 
 
     }
-    
+
+
+    companion object {
+        private const val NOTIFICATION_ID = 112
+        private const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+    }
+
+    private lateinit var notificationManager: NotificationManager
+
+    private fun createNotificationChannel() {
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                "Messages",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.description = "Messages Notification"
+            notificationManager.createNotificationChannel(channel)
+
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+            // creating the notification and its parameters.
+            val builder = NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID).apply {
+                setSmallIcon(R.drawable.dinner_table_icon)
+                setContentIntent(pendingIntent)
+                setContentTitle("DinnerTable")
+                setContentText("Wat eet je vandaag?")
+                setAutoCancel(true)
+                setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            }
+
+// displaying the notification with NotificationManagerCompat.
+            with(NotificationManagerCompat.from(this)) {
+                notify(NOTIFICATION_ID, builder.build())
+            }
+
+/* or you can use the notification manager object.
+notificationManager.notify(NOTIFICATION_ID, notification.build())
+*/
+        }
+    }
+
+
+
 }
+
+
+
 
