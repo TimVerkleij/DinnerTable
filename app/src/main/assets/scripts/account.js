952be8feedc1b2abc30c8c5732915fee8889a6aa
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-06-15 12:23:23 +0200

    Add info text to account page

--- app/src/main/assets/scripts/account.js
+++ app/src/main/assets/scripts/account.js
@@ -23,91 +23,109 @@ var editBirthDay = document.getElementById("editBirthDay")
 var editAllergies = document.getElementById("editAllergies")
 var editPreferences = document.getElementById("editPreferences")
 
-    var docRef = db.collection("userProfiles").doc(userID);
+var docRef = db.collection("userProfiles").doc(userID);
 
-    docRef.get().then(function(doc) {
-        if (doc.exists) {
-            db.collection("userProfiles").doc(userID)
-                .onSnapshot(function(doc) {
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+        db.collection("userProfiles").doc(userID)
+            .onSnapshot(function(doc) {
 
-                    //get profile info from database
-                    userInfo = doc.data().userInfo
-
-
+                //get profile info from database
+                userInfo = doc.data().userInfo
 
 
 
-                    var userPhoto = userInfo.Photo
-                    var userName = userInfo.Name
-                    var userBirthday = moment(userInfo.Birthday.toDate()).format("D MMMM YYYY")
-                    var userEmail = userInfo.Email
-                    var userAllergies = userInfo.Allergies
-                    var userPreferences = userInfo.Preferences
-
-                    if (userAllergies == "") {
-                        userAllergies = "Geen"
-                    }
-                    if (userPreferences == "") {
-                        userPreferences = "Geen"
-                    }
-
-                    userProfileImage.src = userPhoto
-                    userNameField.innerHTML = userName
-                    userBirthdayField.innerHTML = userBirthday
-                    userEmailField.innerHTML = userEmail
-                    userAllergiesField.innerHTML = userAllergies
-                    userPreferencesField.innerHTML = userPreferences
-
-                    editUserPhoto.src = userPhoto
-                    editName.value = userName
-                    editBirthDay.value = moment(userInfo.Birthday.toDate()).format("YYYY-MM-DD")
-                    editEmail.value = userEmail
-                    editAllergies.value = userAllergies
-                    editPreferences.value = userPreferences
-
-                })
-        } else {
 
 
-            if (typeof Android != 'undefined') {
+                var userPhoto = userInfo.Photo
+                var userName = userInfo.Name
+                var userBirthday = moment(userInfo.Birthday.toDate()).format("D MMMM YYYY")
+                var userEmail = userInfo.Email
+                var userAllergies = userInfo.Allergies
+                var userPreferences = userInfo.Preferences
 
-                //create document for new user
+                if (userAllergies == "") {
+                    userAllergies = "Geen"
+                }
+                if (userPreferences == "") {
+                    userPreferences = "Geen"
+                }
 
                 userProfileImage.src = userPhoto
                 userNameField.innerHTML = userName
+                userBirthdayField.innerHTML = userBirthday
                 userEmailField.innerHTML = userEmail
+                userAllergiesField.innerHTML = userAllergies
+                userPreferencesField.innerHTML = userPreferences
 
                 editUserPhoto.src = userPhoto
                 editName.value = userName
+                editBirthDay.value = moment(userInfo.Birthday.toDate()).format("YYYY-MM-DD")
                 editEmail.value = userEmail
-                    // editAllergies.value = "test"
-                    // editPreferences.value = "test"
+                editAllergies.value = userAllergies
+                editPreferences.value = userPreferences
+
+            })
+    } else {
 
 
-                let userInfo = {
-                    Name: userName,
-                    Email: userEmail,
-                    Photo: userPhoto,
-                    ID: userID
-                }
+        if (typeof Android != 'undefined') {
 
-                // let user = {
-                //     [userID]: userInfo
-                // }
+            //create document for new user
 
-                function uploadData() {
-                    db.collection("userProfiles").doc(userID).set({
-                        userInfo
-                    }, { merge: true })
-                }
+            userProfileImage.src = userPhoto
+            userNameField.innerHTML = userName
+            userEmailField.innerHTML = userEmail
 
-                uploadData()
+            editUserPhoto.src = userPhoto
+            editName.value = userName
+            editEmail.value = userEmail
+                // editAllergies.value = "test"
+                // editPreferences.value = "test"
+
+
+            let userInfo = {
+                Name: userName,
+                Email: userEmail,
+                Photo: userPhoto,
+                ID: userID
             }
 
+            // let user = {
+            //     [userID]: userInfo
+            // }
 
+            function uploadData() {
+                db.collection("userProfiles").doc(userID).set({
+                    userInfo
+                }, { merge: true })
+            }
 
-
+            uploadData()
         }
-    }).catch(function(error) {
-        console.error("Error getting document:", error);
-    });
+
+
+
+
+    }
+}).catch(function(error) {
+    console.error("Error getting document:", error);
+});
+
+
+function allergiesInfo() {
+    document.getElementById("darkBackground").style.display = "block"
+    document.getElementById("infoDivAllergies").style.display = "block"
+}
+
+function preferenceInfo() {
+    document.getElementById("darkBackground").style.display = "block"
+    document.getElementById("infoDivPreferences").style.display = "block"
+
+}
+
+function exitDarkBackground() {
+    document.getElementById("darkBackground").style.display = "none"
+    document.getElementById("infoDivAllergies").style.display = "none"
+    document.getElementById("infoDivPreferences").style.display = "none"
+}
